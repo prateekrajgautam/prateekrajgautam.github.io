f7dd34ed4878502a9242e429cc645a797a96a6f2
prateekrajgautam <prateekrajgautam@gmail.com>
2024-07-27 10:09:27 +0000

    Auto Update from resume action

--- V01/pdftoppm.sh
+++
@@ -1,8 +0,0 @@
-nix-shell -p poppler_utils --run 'PDF="./Dr.PrateekRajGautam_Resume_2024_V01.pdf" && echo "Converting "$PDF" to image" && pdftoppm -png -r 300 $PDF PNG && echo "\n Command Completed"'
-
-sudo apt install poppler-utils -y
-nix-shell -p poppler_utils
-PDF="./Dr.PrateekRajGautam_Resume_2023_V01.pdf"
-echo "Converting "$PDF" to image"
-pdftoppm -png -r 300 $PDF PNG
-echo "\n Command Completed"
